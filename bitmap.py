fbc1569f9365accf3871c925400b583610cd76f4
nat-chan <qzrp01@gmail.com>
2016-07-23 22:21:39 +0900

    o

--- bitmap.py
+++ bitmap.py
@@ -11,17 +11,28 @@ import wx
 class myFrame(wx.Frame):
 	def __init__(self, parent, title):
 		wx.Frame.__init__(self, parent, title=title)
-		self.Bind(wx.EVT_PAINT, self.OnPaint)
-		im = Image.open('data02053e.png').convert("RGBA")
-		mt = np.asarray(im)
-		self.bitmap = \
-		wx.BitmapFromBufferRGBA(mt.shape[1], mt.shape[0], mt.tostring())
+		self.timer = wx.Timer(self)
+		self.Bind(wx.EVT_TIMER, self.OnTimer)
+#		self.Bind(wx.EVT_PAINT, self.OnPaint)
+		image = Image.open('out.png').convert("RGBA")
+		self.array = np.asarray(image)
+		self.counter = -1
+		self.SetSize((55, 200))
+		self.timer.Start(6)
 
-		self.SetSize((mt.shape[1], mt.shape[0]))
-
-	def OnPaint(self, event=None):
-		dc = wx.PaintDC(self)
-		dc.DrawBitmap(self.bitmap, 0, 0, True)
+	def OnTimer(self, event):
+		self.counter += 1
+		i = self.counter
+		part = self.array[-1-200-i:-1-i,:,:]
+		if part.shape == (200, 55, 4):
+			bitmap = wx.BitmapFromBufferRGBA(55, 200, part.tostring())
+			dc = wx.PaintDC(self)
+			dc.DrawBitmap(bitmap, 0, 0, True)
+		else:
+			self.timer.Stop()
+#	def OnPaint(self, event=None):
+#		dc = wx.PaintDC(self)
+#		dc.DrawBitmap(self.bitmap, 0, 0, True)
 
 app = wx.App(False)
 frame = myFrame(None, "sdvx")
